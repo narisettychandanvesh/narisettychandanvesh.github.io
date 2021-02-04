698180672d55a9facedf969b3b191a0a735747cb
narisettychandanvesh <78471537+narisettychandanvesh@users.noreply.github.com>
2021-02-04 16:29:38 +0530

    Add files via upload

---
+++ Bubble sort.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+using namespace std;
+int main()
+{
+int num,i,arr[50],j,temp;
+cout<<"\n enter total number of elements";
+cin>>num;
+cout<<"\nEnter"<<num<<"Elements:\n\n";
+for(i=0;i<num;i++)
+{
+cin>>a[i];
+}
+//logic
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n-1;j++)
+{
+if(a[j]>a[j+1])
+{
+temp=a[j];
+a[j+1]=temp:
+}
+}
+}
+cout<<"\n sorted elements in ascending order:\n";
+for{i=0;i<num;i++}
+{
+cout<<"";
+cout<<arr[i]<<"";
+}
+return0;
+}
